6d1c9fd46ddbc110244efb4ec03c9f7fce5b4268
mo6709 <mo6709@gmail.com>
2016-11-11 11:02:47 -0500

    created Scraper #crape_homeFinder_page

--- lib/realestate_by_zipcode/scraper.rb
+++ lib/realestate_by_zipcode/scraper.rb
@@ -1,6 +1,6 @@
 class RealestateByZipcode::Scraper
   #this class scrape www.homefinder.com
-  attr_accessor :zipcode, :property_style 
+  attr_accessor :zipcode, :property_style, :doc 
 
   def initialize(zipcode, property_style)
 
@@ -12,6 +12,12 @@ class RealestateByZipcode::Scraper
 	def get_homeFinder_page
 
 		@doc = Nokogiri::HTML(open("http://www.homefinder.com/zip-code/#{@zipcode}/#{@property_style}/")) 
+
+	end
+
+	def scrape_homeFinder_page 
+
+		@doc.css("div#leftColumn div.resultsBands.last")
 
 	end
 
