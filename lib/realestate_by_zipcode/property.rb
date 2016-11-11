02f51ce4d95136024a698d72c58868699dfa1937
mo6709 <mo6709@gmail.com>
2016-11-11 14:30:51 -0500

    created Property #.add_property, #properties

--- lib/realestate_by_zipcode/property.rb
+++ lib/realestate_by_zipcode/property.rb
@@ -2,27 +2,45 @@ class RealestateByZipcode::Property
 	
 	attr_accessor :price, :lcation, :url, :beds, :baths, :year_built, :property_size, :floors, :last_transaction, :last_transaction_purchase_price
   
-  @@all = []
+  @@properties = []
 
 	def initialize(location=nil, price=nil, url=nil)
 
   	@location = location
   	@price = price
   	@url = url
-   
-    # add_property(self)
+    
+    self.class.add_property(self)
 
 	end
   
   def self.new_from_index_page(property)
 
 		location = property.css("span[itemprop='name']").text + property.css("div.cityStZip").text #=> it should return the location
-		price = property.css("div.price").text.strip #=> it should return a price
-		url = property.css("div.alignForTwoPhotos a").attribute("href").value #=> it should return a url
+		price = property.css("div.price").text.strip                                               #=> it should return a price
+		url = property.css("div.alignForTwoPhotos a").attribute("href").value                      #=> it should return a url
 
 		self.new(location, price, url)
 
   end
  
+
+
+  def self.properties
+
+  	@@properties.dup.freeze
+
+  end
+
+  def self.add_property(property)
+	 
+	  if !property.is_a?(RealestateByZipcode::Property)
+	    raise "invalid property" 
+	  else   
+	    @@properties << property
+	  end  
+
+  end
+
 
 end
