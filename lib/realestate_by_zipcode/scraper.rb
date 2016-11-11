be5c2e1d5ae2aba22f5c604b225cb1503ac035a7
mo6709 <mo6709@gmail.com>
2016-11-11 09:41:26 -0500

    created Scraper #initialize

--- lib/realestate_by_zipcode/scraper.rb
+++ lib/realestate_by_zipcode/scraper.rb
@@ -1,4 +1,12 @@
 class RealestateByZipcode::Scraper
   
+  attr_accessor :zipcode, :property_style 
+
+  def initialize(zipcode, property_style)
+  	
+  	@zipcode = zipcode
+  	@property_style = property_style
+  	
+  end
 
 end
