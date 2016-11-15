4b5351b20829624feea2971041e86f670696c371
mo6709 <mo6709@gmail.com>
2016-11-15 17:00:41 -0500

    add design to CLI

--- lib/realestate_by_zipcode/cli.rb
+++ lib/realestate_by_zipcode/cli.rb
@@ -6,7 +6,9 @@ class RealestateByZipcode::CLI
 		                   "farm", "houseboat", "commercial", "building-lot", "raw-land"] 
 
 	def call
+		puts "*****************************"
 		puts "Welcom to RealestateByZipcode"
+		puts "*****************************"
 	  start
     goodbey
 	end
@@ -17,46 +19,52 @@ class RealestateByZipcode::CLI
     
     property_index = "" 
     while property_index != "exit" 
-      puts "-------------------Property's Details-------------------"
+      puts "---------------------Property's Details---------------------"
       puts "Please enter the property number for more details or 'exit' "
+      puts "------------------------------------------------------------"
       property_index = gets.strip
       display_property(property_index)
     end
-
+    puts "----------------------------------------------"
     puts "Would you like to explore a different zipcode?"
+    puts "----------------------------------------------"
     start_over = gets.strip
     start if start_over.downcase == "yes"  
 	end
 
 	def initialize_properties  
     RealestateByZipcode::Property.delete_properties
-
+    puts "-------------------------------------"
 	  puts "What Zip-Code would you like explore?"
+	  puts "-------------------------------------"
 		zipcode = gets.strip
 
     if zipcode != "exit"
+    	puts "------------------------------------------"
 	    puts "What type of property are you looking for?"
 	    puts "------------------------------------------"
-		  puts "Single Family Home        enter - 1"
-		  puts "Multi-Family Home         enter - 2"
-		  puts "Duplex                    enter - 3"
-		  puts "Townhouse/Townhome         enter - 4"
-		  puts "Co-op unit                enter - 5"
-		  puts "Row House                 enter - 6"
-		  puts "Condominium/Apartment     enter - 7"
-		  puts "Patio Home                enter - 8"
-		  puts "Mobile/Manufactured Home  enter - 9"
-		  puts "Farm                      enter - 10"
-		  puts "Houseboat                 enter - 11"
-		  puts "Commercial Building       enter - 12"
-		  puts "Building Lot              enter - 13"
-		  puts "Raw Land                  enter - 14"
+		  puts "| Single Family Home          enter - 1  |"
+		  puts "| Multi-Family Home           enter - 2  |"
+		  puts "| Duplex                      enter - 3  |"
+		  puts "| Townhouse/Townhome          enter - 4  |"
+		  puts "| Co-op unit                  enter - 5  |"
+		  puts "| Row House                   enter - 6  |"
+		  puts "| Condominium/Apartment       enter - 7  |"
+		  puts "| Patio Home                  enter - 8  |"
+		  puts "| Mobile/Manufactured Home    enter - 9  |"
+		  puts "| Farm                        enter - 10 |"
+		  puts "| Houseboat                   enter - 11 |"
+		  puts "| Commercial Building         enter - 12 |"
+		  puts "| Building Lot                enter - 13 |"
+		  puts "| Raw Land                    enter - 14 |"
+		  puts "------------------------------------------"
 		  input = gets.strip
 
 	    property_type = input_evaluation(input)
 	    if RealestateByZipcode::Scraper.call(zipcode, property_type) == "404 Not Found"
-	    	puts "We're sorry. No properties match your search :("
-	    	puts ""
+	    	puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	    	puts " Sorry. No properties match your search :( "
+	    	puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 	    	initialize_properties
 	    end
 	  end
@@ -64,17 +72,18 @@ class RealestateByZipcode::CLI
 	end
 
 	def display_properties
+		puts "===================================================================================="
 	  RealestateByZipcode::Property.properties.each.with_index(1) do |p, i|
-	  	puts "#{i}. #{p.price}   - #{p.location}"
+	  	puts "| #{i}. #{p.price}   - #{p.location}"
 	  end
-    puts ""
+    puts "===================================================================================="
 	end
 
 	def display_property(index)
 
     if index.to_i > 0  && index.to_i <= RealestateByZipcode::Property.properties.size
 			p = RealestateByZipcode::Property.properties[index.to_i-1]
-	    puts "----------------------#{p.location}----------------------"
+	    puts "========================#{p.location}=========================="
 	    puts "Price:                           #{p.price}"
 	    puts "Beds:                            #{p.beds}"
 	    puts "Baths:                           #{p.baths}"
@@ -85,14 +94,18 @@ class RealestateByZipcode::CLI
 	    puts "Last transaction:                #{p.last_transaction}"
 	    puts "Last transaction purchase price: #{p.last_transaction_purchase_price}"
 	    puts ""
+	    puts "------------------------------------------"
 	    puts "Would you like to see a brief description?"
+	    puts "------------------------------------------"
 	    input = gets.strip
 	    if input.downcase == "yes" 
 	      puts "#{p.description}"
 	      puts ""
 	    end 
 	  else
-	    puts "Please enter a valid number"  
+      puts "---------------------------"
+	    puts "Please enter a valid number"
+	    puts "---------------------------"  
 	  end
 	end
 
@@ -101,7 +114,9 @@ class RealestateByZipcode::CLI
 	end
   
   def goodbey
-  	puts "---- GOODBEY! ----"
+  	puts "*******************"
+  	puts "     GOODBEY! "
+  	puts "*******************"
   end
 
 end
